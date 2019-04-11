e6e64206a08071786769ed2637bc70b36e1fbed3
Eric <Eric.Curley94@gmail.com>
2019-04-11 14:56:33 -0400

    moved function to other file

--- public/app.js
+++ public/app.js
@@ -13,30 +13,6 @@ firebase.firestore().enablePersistence()
 var db = firebase.firestore();
 var articles = db.collection('articles')
 
-
-function fillInterface(){
-    document.getElementById('container').innerHTML = 
-    `
-    <label>Title: </label><input type="text" name="title" id="title"><br>
-    <label>Authors: </label><input type="text" name="authors" id="authors"><br>
-    <label>Summary: </label><textarea name="summary" rows="10" cols="30" id="summary"></textarea><br>
-    <input type="button" value="Submit" name="submit" id="submit" onclick="submitValues()">
-        
-        
-    <table id="table_id" class="display">
-        <thead>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-    `
-}
-
-
-
-
-
-
 function submitValues() {
 
     if(firebase.auth().currentUser == undefined){
